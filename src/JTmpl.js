331f01d2c8149048e057f9f524d1c7c8212c86e2
Jonathan Sanchez <jonathansanchez@me.com>
2012-02-11 21:55:38 +0100

    Call attr added, register function

--- src/JTmpl.js
+++ src/JTmpl.js
@@ -3,7 +3,7 @@
 
 	$(function(){
 		$('script[type=jtmpl]').each(function() {
-			JTmpl.register(this.id, '<div>' + this.innerHTML + '</div>');
+			JTmpl.register(this.id, this.innerHTML);
 		});
 	});
 
@@ -12,46 +12,62 @@
 		
 	}
 
-	JTmpl.register = function(id,html) {
-		templates[id] = $(html);
+	JTmpl.register = function(id, tmpl) {
+		if(typeof tmpl === 'string'){
+			templates[id] = $('<div>' + tmpl + '</div>');
+		} else {
+			templates[id] = tmpl;
+		}
 	}
 	JTmpl.call = function(id,data) {
-		return transform(templates[id].clone(),data);
+		var tmpl = templates[id];
+		if(tmpl instanceof jQuery){
+			return transform(tmpl.clone(), data).children();
+		} else {
+			return tmpl(data);
+		}
 	}
 	
 	function transform($elem,data) {
-		console.log('transform', data);
-		$elem.find('*[data-jtmpl-each]').each(function() {
+		$elem.find('*[data-jtp-each]').each(function() {
 			var $this = $(this);
 			console.log('each', this);
-			if(!$this.parents('*[data-jtmpl-each]').length) {
-				data.q($this.data('jtmplEach'),function(i,e) {
-					var res = transform($this.clone().removeAttr('data-jtmpl-each'), e)
-					console.log('res',res)
-					$this.after(res);
+			if(!$this.parents('*[data-jtp-each]').length) {
+				data.q($this.data('jtpEach'),function(i,e) {
+					var res = transform($this.clone().removeAttr('data-jtp-each'), e)
+					$this.before(res);
 				})
 			}
 			$this.remove();
 		});
 		
-		$elem.find('*[data-jtmpl-select], test*[data-jtmpl-test]').each(function() {
-			var $this = $(this);
-			if(!$this.parents('*[data-jtmpl-each]').length) {
-				if($this.data('jtmplTest')) {
-					test = data.q($this.data('jtmplTest'));
-					if(!test) {
-						$this.remove();
-					}
-				}
-				if($this.data('jtmplSelect')) {
-					var selector = $this.data('jtmplSelect')
-					var value = data.q(selector).valueOf()
-					console.log(selector, '=', value)
-					$this.html(value).removeAttr('data-jtmpl-select');
-				}
-				console.log(this)
+		$elem.find('*[data-jtp-test]').each(function() {
+			var $this = $(this),
+			 	selector = $this.data('jtp-test'),
+				test = data.q(selector);
+			
+			if(!test.length || !test[0]) {
+				$this.remove();
 			}
+			$this.removeAttr('data-jtp-test');
 		});
+		
+		$elem.find('*[data-jtp-select]').each(function() {
+			var $this = $(this),
+			 	selector = $this.data('jtp-select'),
+			 	value = data.q(selector).valueOf();
+			
+			$this.html(value).removeAttr('data-jtp-select');
+		});
+	
+		$elem.find('*[data-jtp-call]').each(function() {
+			var $this = $(this),
+			 	tmpl = $this.data('jtp-call'),
+			 	value = JTmpl.call(tmpl, data);
+
+			$this.html(value).removeAttr('data-jtp-call');
+		});
+
 		return $elem;
 	}
 	
--- test/test.js
+++ test/test.js
@@ -1,10 +1,77 @@
-module('add')
-test('',function(){
-	data = new JPath({
-		directory:[{
-			firstname:'Steven',
-			lastname:'Test'
-		}]
+module('basic')
+var data = new JPath({
+	directory:{
+		testfamily:[{
+				firstname:'Steven',
+				lastname:'Test'
+			},{
+				firstname:'Brian',
+				lastname:'Test',
+				category:'work'
+			},{
+				firstname:'Kely',
+				lastname:'Test'
+			},{
+				firstname:'Jason',
+				lastname:'Test'
+			}],
+		'ateam':[{
+				nickname:'Hannibal',
+				actor:'George Peppard',
+				rank:'Colonel'
+			},{
+				nickname:'Futé',
+				actor:'Dirk Benedict',
+				rank:'Lieutenant'
+			},{
+				nickname:'Looping',
+				actor:'Dwight Schultz',
+				rank:'Capitaine'
+			},{
+				nickname:'Barracuda',
+				actor:'Mister T',
+				rank:'Sergent'
+			}]
+		
+	}
+});
+test('Basic template',function(){
+
+	JTmpl.register('example1','<div>\
+		<p data-jtp-each="directory/testfamily">\
+    		<span data-jtp-select="firstname">Default value</span>\
+    		<strong data-jtp-select="lastname">Default value</strong>\
+			<span data-jtp-test="category=\'work\'">(work)</span>\
+		</p>\
+	</div>');
+	$res = JTmpl.call('example1',data);
+	$('#test-fixture').append($res);
+	equals($res.find('p').length, 4);
+	equals($res.find('p:nth-child(1) *:nth-child(1)').text(), 'Steven');
+	// equals($res.find('p:nth-child(2)').children().length, 3);
+	equals($res.find('p:nth-child(3) *:nth-child(2)').text(), 'Test');
+});
+
+test('Basic template 2',function(){
+	expect(7);
+	JTmpl.register('actor',
+		'<span data-jtp-select="actor">nobody</span>');
+	JTmpl.register('function',function(){
+		ok(true);
+		return 'function called';
 	});
-	$('#test-fixture').append(JTmpl.call('example1',data));
-})
+	JTmpl.register('example2','<div>\
+		<p data-jtp-each="directory/ateam">\
+    		<strong data-jtp-select="nickname">None</strong>\
+    		(<span data-jtp-call="actor">nobody</span>)\
+			<span data-jtp-select="rank">soldier</span>\
+			<span data-jtp-call="function">test</span>\
+		</p>\
+	</div>');
+	$res = JTmpl.call('example2',data);
+	$('#test-fixture').append($res);
+	equals($res.find('p').length, 4);
+	equals($res.find('p:nth-child(1) > *:nth-child(1)').text(), 'Hannibal');
+	// equals($res.find('p:nth-child(2)').children().length, 3);
+	equals($res.find('p:nth-child(3) > *:nth-child(3)').text(), 'Capitaine');
+});
