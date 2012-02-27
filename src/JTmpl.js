7eecd6783addf4fe58977cc742d772ac70694ffb
Jonathan Sanchez <jonathansanchez@me.com>
2012-02-27 22:08:28 +0100

    demo evolved

--- src/JTmpl.js
+++ src/JTmpl.js
@@ -54,6 +54,24 @@
 		//attr
 		//retain
 		
+		$elem.find('*[data-jtp-attr]').each(function() {
+			var $this = $(this),
+			 	attr = $this.data('jtp-attr');
+			if(typeof attr == 'string') {
+			 	var select = $this.data('jtp-select'),
+			 		value = data.q(select) || '';
+				$this.attr(attr, value).removeAttr('data-jtp-attr').removeAttr('data-jtp-select');
+			} else if(typeof attr == 'object') {
+				//json
+				for(var name in attr) {
+					var value = data.q(attr[name]) || '';
+					$this.attr(name, value);
+				}
+				$this.removeAttr('data-jtp-attr');
+			}
+
+		});
+
 		$elem.find('*[data-jtp-call]').each(function() {
 			var $this = $(this),
 			 	tmpl = $this.data('jtp-call'),
